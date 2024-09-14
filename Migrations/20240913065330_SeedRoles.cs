5710d87928052d712a116a19fab5e28340b305e7
VxyX <32436840+VxyX@users.noreply.github.com>
2024-09-14 10:02:17 +0900

    User Register

---
+++ Migrations/20240913065330_SeedRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "da8b26ef-4d35-46ae-b3d4-57d22248a9a1", null, "User", "USER" },
+                    { "f8d6492e-5707-456f-b4bf-111a6658dcce", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "da8b26ef-4d35-46ae-b3d4-57d22248a9a1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f8d6492e-5707-456f-b4bf-111a6658dcce");
+        }
+    }
+}
