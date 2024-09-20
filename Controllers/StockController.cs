b96232bb1bd694c10a0c1cefed341a3c69b57012
VxyX <32436840+VxyX@users.noreply.github.com>
2024-09-20 09:23:53 +0900

    JWT Token + Login

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -7,6 +7,7 @@ using api.Dtos.Stock;
 using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 //this is controller: manage the api endpoint and its function
@@ -27,6 +28,7 @@ namespace api.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if(!ModelState.IsValid)
